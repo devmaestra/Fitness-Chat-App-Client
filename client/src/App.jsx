c431d18708277113a4e0249456845bc6af9bde83
mccarthygalaxy <124395340+mccarthygalaxy@users.noreply.github.com>
2023-10-03 21:38:10 -0400

    Layout of Basic App, mounting Header, Footer, Nav into page; edited index.html, index.js and other to accept more React; ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
-import "./App.css";
-import React from "react";
+import './App.css';
+import React from 'react';
 import ProfilePic from "./components/profile/ProfilePic";
 
+// import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
 
 function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ProfilePic />
+      <Nav />
+      {/* <Footer /> */}
 
-  return <div className="App">Swoulmates
-  <ProfilePic />
-
-  </div>
+    </div>
+  );
 }
 
 export default App;
