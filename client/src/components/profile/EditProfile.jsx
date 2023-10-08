c3637c1fb59d8c6efc73f28dd2d3f4be66bf41b5
SinGer ZS <singer.nzs@gmail.com>
2023-10-08 10:34:16 -0400

    Edit Profile Update

--- client/src/components/profile/EditProfile.jsx
+++ client/src/components/profile/EditProfile.jsx
@@ -4,182 +4,169 @@ import { baseURL } from '../../utils';
 
 function EditProfile(props) {
 
-/*     const [traits, setRooms] = useState([]);
+/*     const emailRef = useRef();
+    const passwordRef = useRef();
 
-    const editProfile = async () => {
-        const url = `${baseURL}/profile`
-    };
+    const navigate = useNavigate();
 
-    const requestOption = {
-        method: 'GET',
-        headers: new Headers({
-            "Authorization": props.token
-        })
-    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
 
-    try {
-
-        const res = await fetch(url, requestOption);
-        const data = await res.json();
-
-        setUser(data.getAllRooms)
-
-    } catch (err) {
-        console.error(err.message)
+        const url = 'http://localhost:4001/user/profile'
     } */
 
-return (
+    return (
 
-    <Form /* onSubmit={handleSubmit} */>
-        <FormGroup>
-            <Label for="exampleEmail">
-                Email
-            </Label>
-            <Input
-                id="exampleEmail"
-                name="email"
-                placeholder="with a placeholder"
-                type="email"
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="examplePassword">
-                Password
-            </Label>
-            <Input
-                id="examplePassword"
-                name="password"
-                placeholder="password placeholder"
-                type="password"
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleSelect">
-                Select
-            </Label>
-            <Input
-                id="exampleSelect"
-                name="select"
-                type="select"
-            >
-                <option>
-                    1
-                </option>
-                <option>
-                    2
-                </option>
-                <option>
-                    3
-                </option>
-                <option>
-                    4
-                </option>
-                <option>
-                    5
-                </option>
-            </Input>
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleSelectMulti">
-                Select Multiple
-            </Label>
-            <Input
-                id="exampleSelectMulti"
-                multiple
-                name="selectMulti"
-                type="select"
-            >
-                <option>
-                    1
-                </option>
-                <option>
-                    2
-                </option>
-                <option>
-                    3
-                </option>
-                <option>
-                    4
-                </option>
-                <option>
-                    5
-                </option>
-            </Input>
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleText">
-                Text Area
-            </Label>
-            <Input
-                id="exampleText"
-                name="text"
-                type="textarea"
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleFile">
-                File
-            </Label>
-            <Input
-                id="exampleFile"
-                name="file"
-                type="file"
-            />
-            <FormText>
-                This is some placeholder block-level help text for the above input. It‘s a bit lighter and easily wraps to a new line.
-            </FormText>
-        </FormGroup>
-        <FormGroup tag="fieldset">
-            <legend>
-                Radio Buttons
-            </legend>
-            <FormGroup check>
-                <Input
-                    name="radio1"
-                    type="radio"
-                />
-                {' '}
-                <Label check>
-                    Option one is this and that—be sure to include why it‘s great
+        <Form /* onSubmit={handleSubmit} */>
+            <FormGroup>
+                <Label for="exampleEmail">
+                    Email
                 </Label>
-            </FormGroup>
-            <FormGroup check>
                 <Input
-                    name="radio1"
-                    type="radio"
+                    id="exampleEmail"
+                    name="email"
+                    placeholder="with a placeholder"
+                    type="email"
                 />
-                {' '}
-                <Label check>
-                    Option two can be something else and selecting it will deselect option one
-                </Label>
             </FormGroup>
-            <FormGroup
-                check
-                disabled
-            >
+            <FormGroup>
+                <Label for="examplePassword">
+                    Password
+                </Label>
                 <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="password placeholder"
+                    type="password"
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="exampleSelect">
+                    Select
+                </Label>
+                <Input
+                    id="exampleSelect"
+                    name="select"
+                    type="select"
+                >
+                    <option>
+                        1
+                    </option>
+                    <option>
+                        2
+                    </option>
+                    <option>
+                        3
+                    </option>
+                    <option>
+                        4
+                    </option>
+                    <option>
+                        5
+                    </option>
+                </Input>
+            </FormGroup>
+            <FormGroup>
+                <Label for="exampleSelectMulti">
+                    Select Multiple
+                </Label>
+                <Input
+                    id="exampleSelectMulti"
+                    multiple
+                    name="selectMulti"
+                    type="select"
+                >
+                    <option>
+                        1
+                    </option>
+                    <option>
+                        2
+                    </option>
+                    <option>
+                        3
+                    </option>
+                    <option>
+                        4
+                    </option>
+                    <option>
+                        5
+                    </option>
+                </Input>
+            </FormGroup>
+            <FormGroup>
+                <Label for="exampleText">
+                    Text Area
+                </Label>
+                <Input
+                    id="exampleText"
+                    name="text"
+                    type="textarea"
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="exampleFile">
+                    File
+                </Label>
+                <Input
+                    id="exampleFile"
+                    name="file"
+                    type="file"
+                />
+                <FormText>
+                    This is some placeholder block-level help text for the above input. It‘s a bit lighter and easily wraps to a new line.
+                </FormText>
+            </FormGroup>
+            <FormGroup tag="fieldset">
+                <legend>
+                    Radio Buttons
+                </legend>
+                <FormGroup check>
+                    <Input
+                        name="radio1"
+                        type="radio"
+                    />
+                    {' '}
+                    <Label check>
+                        Option one is this and that—be sure to include why it‘s great
+                    </Label>
+                </FormGroup>
+                <FormGroup check>
+                    <Input
+                        name="radio1"
+                        type="radio"
+                    />
+                    {' '}
+                    <Label check>
+                        Option two can be something else and selecting it will deselect option one
+                    </Label>
+                </FormGroup>
+                <FormGroup
+                    check
                     disabled
-                    name="radio1"
-                    type="radio"
-                />
+                >
+                    <Input
+                        disabled
+                        name="radio1"
+                        type="radio"
+                    />
+                    {' '}
+                    <Label check>
+                        Option three is disabled
+                    </Label>
+                </FormGroup>
+            </FormGroup>
+            <FormGroup check>
+                <Input type="checkbox" />
                 {' '}
                 <Label check>
-                    Option three is disabled
+                    Check me out
                 </Label>
             </FormGroup>
-        </FormGroup>
-        <FormGroup check>
-            <Input type="checkbox" />
-            {' '}
-            <Label check>
-                Check me out
-            </Label>
-        </FormGroup>
-        <Button>
-            Submit
-        </Button>
-    </Form>
-)
+            <Button>
+                Submit
+            </Button>
+        </Form>
+    )
 }
 
 export default EditProfile
