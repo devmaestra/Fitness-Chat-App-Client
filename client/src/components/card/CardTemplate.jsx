f177406d435fa7ec5a2c123fa403b9b63225e002
devmaestra <127052615+devmaestra@users.noreply.github.com>
2023-10-08 18:19:41 -0500

    cardTemplates adjusted

--- client/src/components/card/CardTemplate.jsx
+++ client/src/components/card/CardTemplate.jsx
@@ -16,33 +16,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function CardTemplate({ username, userImage, locationZip, cityName }) {
   return (
     <div>
-      <Row>
-        <Col className="col-6">
-          <Card className="flex-fill">
-            <CardImg
-              src={userImage || "/assets/User-Profile-PNG-Image.png"}
-              alt={username}
-            />
-            <CardBody>
-              <CardTitle tag="h1">{username}</CardTitle>
-              <CardSubtitle className="mb-2" tag="h4">
-                Nearby in: {locationZip}
-              </CardSubtitle>
-              <CardText>{cityName}</CardText>
-              <Button
-                className="chat-btn"
-                color="success"
-                onClick="PLACEHOLDER for chat conversation"
-              >
-                Chat with {username}!
-              </Button>
-            </CardBody>
-          </Card>
-        </Col>
-        <br />
+      <Row classname="">
         <Col>
-          <Card className="flex-fill">
+          <Card style={{ margin: "10px" }}>
             <CardImg
+              className="card-image"
+              style={{
+                width: "250px",
+                height: "250px",
+              }}
               src={userImage || "/assets/User-Profile-PNG-Image.png"}
               alt={username}
             />
